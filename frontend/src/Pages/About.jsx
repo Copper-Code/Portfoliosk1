1aff5a3d2d97be4372245be5d5d23c33b21d5e55
Copper Code <123915577+Copper-Code@users.noreply.github.com>
2026-02-24 11:25:52 +0700

    Edit Size

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -14,21 +14,21 @@ function About() {
           </Card.Header>
           <Card.Body>
             <blockquote className="blockquote mb-0">
-              <p>2562-2564</p>
-              <footer className="blockquote-footer">
+              <p style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>2562-2564</p>
+              <footer className="blockquote-footer" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
                 วิทยาลัยอาชีวศึกษานครสวรรค์
               </footer>
-              <footer className="blockquote-footer">
+              <footer className="blockquote-footer" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
                 ปวส. สาขาคอมพิวเตอร์ธุรกิจ
               </footer>
             </blockquote>
 
             <blockquote className="blockquote mb-0">
-              <p>2564-2568</p>
-              <footer className="blockquote-footer">
+              <p  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>2564-2568</p>
+              <footer className="blockquote-footer" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
                 มหาวิทยาลัยสุโขทัยธรรมาธิราช
               </footer>
-              <footer className="blockquote-footer">
+              <footer className="blockquote-footer" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
                 วิชาเอกวิทยาการคอมพิวเตอร์ วิชาเฉพาะ เว็บและโมบายเทคโนโลยี
               </footer>
             </blockquote>
@@ -41,39 +41,58 @@ function About() {
           </Card.Header>
           <Card.Body>
             <blockquote className="blockquote mb-0">
-              <p>กุมภาพันธ์ 2565 - กันยายน 2566</p>
-              <footer className="blockquote-footer">
+              <p style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>กุมภาพันธ์ 2565 - กันยายน 2566</p>
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>
                 โรงพยาบาลสวรรค์ประชารักษ์
               </footer>
-              <footer className="blockquote-footer">
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>
                 ตำแหน่ง : เจ้าพนักงานพัสดุ
               </footer>
-              <footer className="blockquote-footer">
-                หน้าที่ : ทำบันทึกขอซื้อขอจ้าง E-GP เบิก จ่าย ยืม,
-                คืนวัสดุ-ครุภัณฑ์, สำรวจ-ทำทะเบียนครุภัณฑ์, แจ้งซ่อมบำรุง,
-                จัดเก็บเอกสารใบสำคัญ, ติดต่อประสานงาน
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>
+                หน้าที่ : 
+                <div className="ms-5" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
+                  <lu>
+                   <li>จัดทำบันทึกขอซื้อขอจ้างผ่านระบบ E-GP</li>
+                  <li>ดำเนินการเบิก–จ่าย ควบคุมการยืม–คืนวัสดุและครุภัณฑ์</li>
+                  <li>สำรวจ ตรวจสอบ และจัดทำทะเบียนคุมครุภัณฑ์</li>
+                  <li>ประสานงานแจ้งซ่อมบำรุงและติดตามสถานะ</li>
+                  <li>จัดเก็บและดูแลเอกสารใบสำคัญทางราชการ</li>
+                  <li>ติดต่อประสานงานกับหน่วยงานภายในและภายนอกที่เกี่ยวข้อง</li>
+                   </lu>
+                   </div>
               </footer>
             </blockquote>
 
             <blockquote className="blockquote mb-0">
-              <p>พฤศจิกายน 2566 - มิถุนายน 2568</p>
-              <footer className="blockquote-footer">ฟรีแลนซ์-แอดมินเพจอสังหาริมทรัพย์</footer>
-              <footer className="blockquote-footer">
-                  ดูแลหน้าเพจ Facebook,
-                  นำเสนอให้ข้อมูลตอบคำถามลูกค้า,
-                  ลงโพสต์ Facebook , Marketplace,
-                  ติดต่อประสานงานลูกค้ากับทีม
+              <p  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>พฤศจิกายน 2566 - มิถุนายน 2568</p>
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>ฟรีแลนซ์-แอดมินเพจอสังหาริมทรัพย์</footer>
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>
+                หน้าที่ :
+                <div className="ms-5" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
+                  <lu>
+                    <li>ดูแลและจัดการเพจพร้อมลงโพสต์ Facebook และ Marketplace </li>
+                    <li>ให้ข้อมูลและตอบคำถามลูกค้า</li>
+                    <li>ประสานงานลูกค้ากับทีม</li>
+                  </lu>
+                </div>
               </footer>
             </blockquote>
 
             <blockquote className="blockquote mb-0">
-              <p>มิถุนายน 2568-ธันวาคม 2568</p>
-              <footer className="blockquote-footer">Succeed Cafe</footer>
-              <footer className="blockquote-footer">ตำแหน่ง : แคชเชียร์</footer>
-              <footer className="blockquote-footer">
-                หน้าที่ : บริการลูกค้า (รับออร์เดอร์/รับชำระเงิน/เสิร์ฟ),
-                ดูแลความสะอาดเรียบร้อย, เตรียมอุปกรณ์, เพิ่มข้อมูลสินค้าลงระบบหลังบ้าน
-                Gpos ดูแลเพจ Facebook, Line ทำสื่อประชาสัมพันธ์ (ป้าย,โบรชัวร์)
+              <p  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>มิถุนายน 2568-ธันวาคม 2568</p>
+              <footer className="blockquote-footer" style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>Succeed Cafe</footer>
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>ตำแหน่ง : แคชเชียร์</footer>
+              <footer className="blockquote-footer"  style={{ fontSize: "clamp(15px, 3vw, 20px)" }}>
+                หน้าที่ :
+                <div className="ms-5" style={{ fontSize: "clamp(14px, 3vw, 18px)" }}>
+                  <lu>
+                    <li>ให้บริการลูกค้า รับออร์เดอร์ รับชำระเงิน</li>
+                    <li>ดูแลความเรียบร้อยภายในร้าน</li>
+                    <li>จัดเตรียมอุปกรณ์และสนับสนุนการดำเนินงานหน้าร้าน</li>
+                    <li>เพิ่มข้อมูลสินค้าในระบบหลังบ้าน (POS) , Grab Merchant</li>
+                    <li>ดูแลเพจ Facebook / Line พร้อมจัดทำสื่อประชาสัมพันธ์</li>
+                  </lu>
+                </div>
               </footer>
             </blockquote>
           </Card.Body>
--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -12,10 +12,15 @@ function Contact() {
             <h4>Contact</h4>
           </Card.Header>
           <Card.Body>
-            <blockquote className="blockquote mb-0">
+            <blockquote
+              className="blockquote mb-0"
+              style={{ fontSize: "clamp(15px, 3vw, 20px)" }}
+            >
               <p>สุธิดา ขำขุน (เบส)</p>
 
-               <footer className="blockquote-footer">Email: sutidakhamkhun@gmail.com</footer>
+              <footer className="blockquote-footer">
+                Email: sutidakhamkhun@gmail.com
+              </footer>
               <footer className="blockquote-footer">
                 Github:
                 <a
@@ -28,7 +33,13 @@ function Contact() {
                 </a>
               </footer>
               <footer className="blockquote-footer">Phone: 062-156-0695</footer>
-              <footer className="blockquote-footer">298/3 หมู่ที่ 5 ต.สันผักหวาน อ.หางดง จ.เชียงใหม่ </footer>
+              <footer className="blockquote-footer">
+                ที่อยู่ 1 : 298/3 หมู่ที่ 5 ต.สันผักหวาน อ.หางดง
+                จ.เชียงใหม่{" "}
+              </footer>
+              <footer className="blockquote-footer">
+                ที่อยู่ 2 : 2/3 ซอยเพชรบุรี 47 เขตห้วยขวาง กรุงเทพฯ
+              </footer>
             </blockquote>
           </Card.Body>
         </Card>
